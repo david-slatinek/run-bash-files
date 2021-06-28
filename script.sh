d75004d11bb66ae6a3dd2393cc895d10d9024e75
David Slatinek <david.slatinek5@gmail.com>
2021-06-28 13:31:11 +0200

    Add comments to script.sh, replaced arguments order in script.sh, update index.html

--- script.sh
+++ script.sh
@@ -8,8 +8,8 @@ help() {
 	echo -e "\tIf only specific files are required to run, check -r flag\n"
 
 	echo -e "SYNOPSIS"
-	echo -e "\tbash script.sh [path] <flags>"
-	echo -e "\tchmod +x script.sh => ./script.sh [path] <flags>"
+	echo -e "\tbash script.sh <flags> [path]"
+	echo -e "\tchmod +x script.sh => ./script.sh <flags> [path]"
 
 	echo -e "\nFLAGS"
 	echo "-h"
@@ -25,7 +25,7 @@ help() {
 	echo -e "\tQuiet mode. Redirects file output to /dev/null"
 
 	echo "-p"
-	echo -e "\tBefore running a file, print path and filename"
+	echo -e "\tPrint path and filename before running the file"
 
 	echo "-f"
 	echo -e "\tPrint path and filename without running the file"
@@ -40,18 +40,26 @@ help() {
 run() {
 	path=$1
 
+	# -r flag => regex
 	if [[ $2 = true ]]; then
+		# Custom regex
 		regex=".*/$3"
 	else
+		# All .sh files
 		regex=".*/*.sh"
 	fi
 
+	# -R flag => recursive
+	# Finds all files in a specified directory
 	if [[ $4 = true ]]; then
+		# Recursive mode
 		files=$(find "$path" -type f -regex "$regex")
 	else
+		# Only specified directory
 		files=$(find "$path" -maxdepth 1 -type f -regex "$regex")
 	fi
 
+	# -f flag => print path and filename without running the file
 	if [[ $7 = true ]]; then
 		printf "%s\n" "$files"
 		exit 0
@@ -61,30 +69,37 @@ run() {
 	local -i numberOfErrors=0
 
 	for file in $files; do
+		# -p flag => print path and filename before running the file
 		if [[ $6 = true ]]; then
 			echo "Executing $file"
 		fi
 
+		# -q flag => quiet mode
 		if [[ $5 = true ]]; then
 			bash "$file" >/dev/null
 		else
 			bash "$file"
 		fi
 
+		# The last file ran successfully
 		if [[ $? -eq 0 ]]; then
 			((numberOfSuccessful++))
 		else
+			# The last file didn't run successfully
 			((numberOfErrors++))
+			# -e flag => print errors
 			if [[ $9 = true ]]; then
 				echo "Error when running $file"
 			fi
 		fi
 
+		# -q flag => quiet mode
 		if [[ $5 = false ]]; then
 			printf "\n"
 		fi
 	done
 
+	# -s flag => print statistics
 	if [[ $8 = true ]]; then
 		echo -e "\nNumber of scripts: $((numberOfSuccessful + numberOfErrors))"
 		echo -e "Successfully executed scripts: $numberOfSuccessful"
@@ -92,27 +107,10 @@ run() {
 	fi
 }
 
+# Flags were not given or path was not specified
 if [[ -z "$*" ]]; then
 	help
 	exit 0
-fi
-
-for i in "$@"; do
-	if [[ $i == "-h" ]]; then
-		help
-		exit 0
-	fi
-done
-
-path=$1
-shift
-
-if [[ ! -e $path ]]; then
-	echo "The path does not exist"
-	exit 1
-elif [[ ! -d $path ]]; then
-	echo "The path is not a folder"
-	exit 1
 fi
 
 r_flag=false
@@ -153,9 +151,22 @@ while getopts "hr:Rqpfse" opt; do
 		e_flag=true
 		;;
 	*)
+		printf "\n"
+		help
 		exit 1
 		;;
 	esac
 done
+
+shift $((OPTIND - 1))
+path=$1
+
+if [[ ! -e $path ]]; then
+	echo "The path does not exist or was not specified"
+	exit 1
+elif [[ ! -d $path ]]; then
+	echo "The path is not a folder"
+	exit 1
+fi
 
 run "$path" "$r_flag" "$r_arg" "$R_flag" "$q_flag" "$p_flag" "$f_flag" "$s_flag" "$e_flag"
